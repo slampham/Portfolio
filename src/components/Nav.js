8a1ee3f5d8bf5decc35a02c1c49df9dc4fc0d808
slampham <43860771+slampham@users.noreply.github.com>
2021-03-11 21:28:05 -0800

    FInished nav bar kind of

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,51 +1,21 @@
 import React from 'react'
 import useToggle from '../hooks/useToggle'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const StyledNav = styled.nav`
+  height: 5rem;
   position: fixed;
   top: 0;
   right: 0;
   left: 0;
   padding: var(--margin);
   background-color: #0A192F;
-
-  * {
-    font: .85rem 'SF Mono';
-
-    :not(button) {
-      color: #DCFFFF;
-    }
-  }
-`;
-
-const HamburgerButton = styled.button`
-  outline-style: none;
-  border: none;
-  padding: 0;
-  float: right;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-
-  div {
-    height: 2px;
-    width: 40px;
-    background-color: #00BA8D;
-    margin: 4px 0;
-
-    &:nth-of-type(2) {
-      width: 75%;
-    }
-
-    &:nth-of-type(3) {
-      width: 60%;
-    }
-  }
 `;
 
 const Menu = styled.ol`
+  font: .85rem 'SF Mono';
   background-color: #112240;
+  color: #DCFFFF;
 
   position: fixed;
   top: 0;
@@ -62,26 +32,71 @@ const Menu = styled.ol`
   list-style-type: none;
   counter-reset: item;
 
+
+  > * {
+    margin: 2em;
+  }
+
   li {
     &:before {
       content: "0" counter(item) ". ";
       color: #6EFBFF;
     }
     counter-increment: item;
-    margin: 2em;
   }
+
+  button {
+    width: 50%;
+    border-width: 2px;
+  }
+`;
+
+const Hamburger = css`
+  div {
+    height: 2px;
+    width: 40px;
+    background-color: #00BA8D;
+    margin: 4px 0;
+
+    &:nth-of-type(2) {
+      width: 75%;
+    }
+
+    &:nth-of-type(3) {
+      width: 60%;
+    }
+  }
+`;
+
+const X_button = css`
+  color: #00BA8D;
+  font-size: 30px;
+`;
+
+const MenuButton = styled.button`
+  outline-style: none;
+  border: none;
+  position: fixed;
+  right: var(--margin);
+  padding: 0;
+  float: right;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  ${ props => props.menuOpen ? X_button : Hamburger }
 `;
 
 function Nav() {
   const [menuOpen, toggle] = useToggle(false);
 
+  function buttonContent() { //!FIXME: not sure if setting up conditional right
+    return menuOpen ? 'X' : <><div/><div/><div/></>;
+  }
+
   return (
-    <StyledNav >
-      <HamburgerButton onClick={toggle}>  
-        <div/>
-        <div/>
-        <div/>
-      </HamburgerButton>
+    <>
+      <StyledNav />
 
       <Menu menuOpen={menuOpen} >
         <li><a href="#about">About</a></li>
@@ -90,7 +105,11 @@ function Nav() {
         <li><a href="#contact">Contact</a></li>
         <button>Resume</button>
       </Menu>
-    </StyledNav>
+
+      <MenuButton menuOpen={menuOpen} onClick={toggle}>  
+        {buttonContent()}
+      </MenuButton>
+    </>
   )
 }
 
