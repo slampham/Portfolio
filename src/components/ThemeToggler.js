b32c3ebca5b4518b529d3b35451902faff46cf8a
slampham <43860771+slampham@users.noreply.github.com>
2021-03-13 14:22:26 -0800

    Theme toggler working

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import { RiMoonClearFill } from 'react-icons/ri'
-// import { FiSun } from 'react-icons/fi'
+import { FiSun } from 'react-icons/fi'
+import useToggle from '../hooks/useToggle'
 
 const StyledButton = styled.button`
+  color: gray;
   padding: 0;
   height: 100%;
   width: ${ ({width}) => width + 'px'};
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `
 
 function ThemeToggler() {
   const [width, setWidth] = useState(0)
   const ref = useRef(null)
+  const [darkTheme, toggle] = useToggle(true)
 
   useEffect(() => {
-    setWidth(ref.current.clientHeight)
-    console.log(ref.current)
-  }, [width])
+    setWidth(3 * ref.current.clientHeight)
+  }, []) //!FIXME: may want to change to be dependent on height
 
   return (
-    <StyledButton {...{ref, width}} >
-      <RiMoonClearFill/>
+    <StyledButton {...{ref, width, onClick: toggle}} >
+      <RiMoonClearFill color={darkTheme && 'white'} />
+      <FiSun color={!darkTheme && 'yellow'} />
     </StyledButton>
   )
 }
