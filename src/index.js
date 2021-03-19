99156bd7593f24ecb58b40c1045c4713ebed256e
slampham <43860771+slampham@users.noreply.github.com>
2021-03-18 23:21:38 -0700

    index updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { ContextProvider } from './contexts/Context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ContextProvider>
       <App />
-    </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
